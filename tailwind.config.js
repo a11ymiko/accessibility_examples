360ef2c73eb42527e499f3b7f16b2c2a1f56f5bf
Beeze3 <osoroshiigosuto@gmail.com>
2024-05-29 09:33:03 +0200

    Add files via upload
    major changes in homework and styles due to addition of tailwind, addition of 4 new colors in tailwind config, new images added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  content: ["*.html", "./good/*.html", "./bad/*.html", "./internal/*.html"],
+  content: ["*/*.html"],
   theme: {
     colors:{
       white: '#ffffff',
       black: '#000000',
       primary: '#003863',
       secondary:'#35b4ff',
+      gray: '#282828',
+      accens_font: '#003963',
+      light_blue: '#80b1c8',
+      skin_like: '#f6f1ed',
     },
     fontFamily:{
       display:['Poppins','sans-serif'],
